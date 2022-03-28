0ee4c58846ee0fc51172e804d1b6c88a10d20e1e
rene <rene@RC8>
2022-03-28 20:59:03 +0200

    Add project files.

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            count += 1;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
